faec957cb85f805f4aa0628cf5038aaa570872c2
Babarindeolanrewaju2 <babarindeolanrewaju@gmail.com>
2020-07-14 21:30:40 +0100

    genres modified

--- index.js
+++ index.js
@@ -1,35 +1,15 @@
-const debug = require("debug")("app:startup");
-const config = require("config");
-const morgan = require("morgan");
-const helmet = require("helmet");
-const Joi = require("joi");
-const logger = require("./middleware/logger");
-const courses = require("./routes/courses");
-const home = require("./routes/home");
+const mongoose = require("mongoose");
+const genres = require("./routes/genres");
 const express = require("express");
 const app = express();
 
-app.set("view engine", "pug");
-app.set("views", "./views"); // default
+mongoose
+  .connect("mongodb://localhost/vidly")
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch((err) => console.error("Could not connect to MongoDB..."));
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
-app.use(helmet());
-app.use(logger);
-
-app.use("/api/courses", courses);
-app.use("/", home);
-
-// Configuration
-console.log("Application Name: " + config.get("name"));
-console.log("Mail Server: " + config.get("mail.host"));
-console.log("Mail Password: " + config.get("mail.password"));
-
-if (app.get("env") === "development") {
-  app.use(morgan("tiny"));
-  debug("Morgan enabled...");
-}
+app.use("/api/genres", genres);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
